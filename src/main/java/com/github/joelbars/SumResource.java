eb45cc3fce550255a37d0eea4aaa63fe456f1188
Joel Santos <joel.santos@bairesdev.com>
2020-08-08 14:44:17 -0300

    Initial commit

---
+++ src/main/java/com/github/joelbars/SumResource.java
@@ -0,0 +1,21 @@
+package com.github.joelbars;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Path("/sum")
+public class SumResource {
+
+    @ConfigProperty(name = "greeting")
+    String greeting;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return this.greeting;
+    }
+}
---
+++ src/test/java/com/github/joelbars/NativeSumResourceIT.java
@@ -0,0 +1,9 @@
+package com.github.joelbars;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeSumResourceIT extends SumResourceTest {
+
+    // Execute the same tests but in native mode.
+}
